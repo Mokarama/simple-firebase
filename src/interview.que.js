e4582fb933b0030c4005402d7bb09c7362b49114
Mokarama <mokaramaaktershanta@gmail.com>
2025-03-30 12:35:04 +0600

    interview question added

---
+++ src/interview.que.js
@@ -0,0 +1,15 @@
+//Interview Questions 
+/** 
+1.What is Firebase?
+2.Is Firebase Frontend or backend?
+3.What is FireBase architecture?
+4.What are the Features or Firebase ?/ 
+what are the tasks you can accomplish with Firebase?
+5.DiFFerences Between Firebase and mongodb. 
+6.Have you ever used Firebase Database(real time database)?
+7.Can you brieFly explain the github authentication process with Firebase?
+8.which method is used to sign-in the user in Firebase Email/Password authentication?
+9.Authentication VS Authorization 
+10.Can you tall me names oF 3 authentication method?
+11.Which authentication methods have you ever used For you project purpose?
+*/
